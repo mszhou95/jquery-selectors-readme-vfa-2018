e9ae78e28255f5bb7fa61eaf7e2fdcfc19386c4a
Michele Zhou <michele.zhou@gmail.com>
2018-06-29 19:52:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  var paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var lastImage= $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+var ninjaBaby = $('#baby-ninja')
+return ninjaBaby
+}
+
+function divSelector() {
+  var picDivs = $('.pics')
+  return picDivs
+}
+
+function firstListItem() {
+  var itemOne = $('#pic-list ul:first-child')
+  return itemOne
+}
